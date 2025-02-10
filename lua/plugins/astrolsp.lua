3efe64eb34a0f6d0de072fcd91ffb022e9e5f6e9
auro.mohanty <aurogreenindia@gmail.com>
2025-02-10 17:14:04 +0100

    feat(lsp|mason): set up LSPs in mason, configure ruff-pyright combination, set up rust-analyzer

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -22,7 +22,10 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "python",
+          "lua",
+          "rust",
+          "json",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
@@ -39,7 +42,8 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "pyright",
+      "ruff",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
@@ -50,6 +54,7 @@ return {
     handlers = {
       -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
       -- function(server, opts) require("lspconfig")[server].setup(opts) end
+      -- ruff = function(server, opts) require("lspconfig").ruff.setup(opts) end
 
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
