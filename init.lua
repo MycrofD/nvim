19e35290c2f1794b114baae1325b707ab193f35b
auro.mohanty <aurogreenindia@gmail.com>
2025-02-10 17:14:04 +0100

    feat(lsp|mason): set up LSPs in mason, configure ruff-pyright combination, set up rust-analyzer

--- init.lua
+++ init.lua
@@ -17,3 +17,45 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- python, ruff, pyright
+require("lspconfig").ruff.setup {
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+    },
+  },
+}
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then return end
+    if client.name == "ruff" then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = "LSP: Disable hover capability from Ruff",
+})
+require("lspconfig").pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { "*" },
+      },
+    },
+  },
+}
+require("lspconfig").ruff.setup {
+  init_options = {
+    settings = {
+      logLevel = "debug",
+    },
+  },
+}
