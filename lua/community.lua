7a98bb04197bac0fc223eb0b875d6f490151a266
auro.mohanty <aurogreenindia@gmail.com>
2025-04-06 22:23:43 +0200

    feat(plugins|community): add git-blame, neoscroll, markdown-preview, folke snacks, folke noice, zen-mode, twilight, precognition-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -16,10 +16,18 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.java" }, -- add Java language support
   { import = "astrocommunity.lsp.nvim-java" }, -- opt-in to nvim-java for LSP
+  { import = "astrocommunity.completion.cmp-emoji" }, -- emoji completion
   { import = "astrocommunity.completion.copilot-lua-cmp" }, -- copilot-lua-cmp
   { import = "astrocommunity.editing-support.copilotchat-nvim" }, -- copilotchat-nvim
-  { import = "astrocommunity.completion.cmp-emoji" }, -- emoji completion
+  { import = "astrocommunity.editing-support.zen-mode-nvim" }, --zen-mode-nvim, distraction-free coding for Neovim
   { import = "astrocommunity.colorscheme.tokyonight-nvim" }, -- tokyonight colorscheme
   { import = "astrocommunity.colorscheme.catppuccin" }, -- catppuccin colorscheme
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" }, -- markdown-preview-nvim
+  { import = "astrocommunity.git.git-blame-nvim" }, -- git-blame-nvim
+  { import = "astrocommunity.scrolling.neoscroll-nvim" }, -- neoscrooll-nvim
+  { import = "astrocommunity.fuzzy-finder.snacks-picker" }, -- folke/snacks-picker
+  { import = "astrocommunity.utility.noice-nvim" }, -- noice.nvim
+  { import = "astrocommunity.color.twilight-nvim" }, -- twilight.nvim (works well with zen-mode-nvim), that dims inactive portions of the code you're editing
+  { import = "astrocommunity.workflow.precognition-nvim" }, -- precognition.nvim assists with discovering motions (Both vertical and horizontal) to navigate your current buffer
   -- import/override with your plugins folder
 }
