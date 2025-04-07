82bc2364c56f7fdbab41f6c5fbd8405d5647b600
auro.mohanty <aurogreenindia@gmail.com>
2025-04-07 14:03:25 +0200

    feat(plugins|community|languages): add packs for java, bash, docker, fish, python, rust, sql

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,14 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.java" }, -- add Java language support
+  { import = "astrocommunity.lsp.nvim-java" }, -- opt-in to nvim-java for LSP
   -- import/override with your plugins folder
 }
